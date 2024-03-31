ef1192f59a6a789fc253fb978d11316ebce1953f
Shubham Gupta <shubhamgupta2730@gmail.com>
2024-03-31 15:03:45 +0530

    completed basic sequelize setup to create a db.

--- readme.md
+++ readme.md
@@ -10,3 +10,5 @@
     repository/
   -tests[later]
 
+
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const { PORT } = require('./config/serverConfig');
+const bodyParser = require('body-parser');
+
+
 
 // Setup and start the server
 const setupAndStartServer = async()=>{
   const app = express();
+  // Add body parser middleware: this will allow us to access the request body in our routes
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
   });
