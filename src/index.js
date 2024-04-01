11671bad9e2701ec2e046903ee67e086f2515073
Shubham Gupta <shubhamgupta2730@gmail.com>
2024-04-02 00:44:28 +0530

    Added Router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { PORT } = require('./config/serverConfig');
 const bodyParser = require('body-parser');
-const cityRepository = require('./repository/city-repository');
+const ApiRoutes = require('./routes/index');
 
 
 
@@ -11,14 +11,10 @@ const setupAndStartServer = async () => {
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use('/api', ApiRoutes);
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-   
-    const repo = new cityRepository();
-    repo.createCity({name:'new delhi'})
-    .catch((error)=>{
-      console.log(error);
-    });
   });
 }
 setupAndStartServer();
