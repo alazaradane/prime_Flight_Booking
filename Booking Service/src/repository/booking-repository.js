c1664e2e7fc6615b722754f1950eb6640f202a96
Shubham Gupta <shubhamgupta2730@gmail.com>
2024-04-09 15:41:55 +0530

    added createBooking repo code

---
+++ Booking Service/src/repository/booking-repository.js
@@ -0,0 +1,26 @@
+const {Booking} = require('../models/index');
+const {AppError, ValidationError} = require('../utils/errors/index');
+const {StatusCodes} = require('http-status-codes');
+
+class BookingRepository  {
+  async create(data){
+    try {
+      const booking = await Booking.create(data);
+      return booking;
+    } catch (error) {
+        if(error.name === 'SequelizeValidationError'){
+          throw new ValidationError(error);
+        }
+        throw new AppError(
+          'Repository Error',
+           'cannot create Booking',
+           'There was some issue creating the Booking',
+            StatusCodes.INTERNAL_SERVER_ERROR,
+          );
+    }
+  }
+
+  
+}
+
+module.exports = BookingRepository;
