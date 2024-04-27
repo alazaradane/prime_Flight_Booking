4042c180b56ba68d098e338d8a43e06fec8e28e5
Shubham Gupta <shubhamgupta2730@gmail.com>
2024-04-27 17:15:51 +0530

    Added Reminder Service initial setup

---
+++ Reminder Service/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {PORT} = require('./config/server-config')
+
+const setupAndStartServer = async () => {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+  })
+
+}
+
+setupAndStartServer();
